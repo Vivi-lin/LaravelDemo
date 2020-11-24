97ef574690b921fd4da93a5f312466e185809ed1
YC <107b11577@mailst.cjcu.edu.tw>
2020-11-24 14:51:10 +0800

    update

---
+++ resources/views/hello_world.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <title>Hello world!!!</title>
+
+    </head>
+
+    <body>
+
+      Hello World!!!
+      中文也可以
+
+    </body>
+
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello-world', function () {
+
+    return view('Hello_world');
+
+}); 
+
+Route::get('/about_us', function () {
+
+    return view('about_us');
+
+}); 
+
+Route::get('/about_us', function () {
+
+    return view('about_us', ['name' => 'Laravel 6.0 範例']);
+
+});
